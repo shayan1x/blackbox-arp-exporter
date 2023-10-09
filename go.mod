57a53bc91d6b0672c0064e205ce3ff016a617671
Amirshayan Aghamiri <a.aghamiri@digikala.com>
2023-10-09 10:33:29 +0330

    Initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module digikala.com/dkdevops/arp-exporter
+
+go 1.21.1
+
+require (
+	github.com/josharian/native v1.1.0 // indirect
+	github.com/mdlayher/arp v0.0.0-20220512170110-6706a2966875 // indirect
+	github.com/mdlayher/ethernet v0.0.0-20220221185849-529eae5b6118 // indirect
+	github.com/mdlayher/packet v1.1.2 // indirect
+	github.com/mdlayher/socket v0.5.0 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"net/netip"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mdlayher/arp"
+)
+
+const IP_REGEX = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$"
+
+func GenerateMetrics(Duration float64, Success int) string {
+	return fmt.Sprintf(`
+# HELP arp_duration_seconds Returns how long the arp request took to complete in seconds
+# TYPE arp_duration_seconds gauge
+arp_duration_seconds %f
+# HELP arp_success Displays whether or not the arp request was a success
+# TYPE arp_success gauge
+arp_success %d
+`, Duration, Success)
+}
+
+func main() {
+	Address := flag.String("listen-address", ":9230", "The Address used to listen the exporter")
+	InterfaceName := flag.String("interface", "eth0", "The interface name to send packets from")
+	flag.Parse()
+
+	http.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
+		Query := r.URL.Query()
+
+		if !Query.Has("target") {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, "Target parameter is missing")
+			return
+		}
+
+		if !Query.Has("module") {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, "Module parameter is missing (currently the only supported module is arp)")
+			return
+		}
+
+		Timeout := 1000
+
+		if Query.Has("timeout") {
+			Result, err := strconv.Atoi(Query.Get("timeout"))
+			if Result >= 1000 && err == nil {
+				Timeout = Result
+			}
+		}
+
+		Target := Query.Get("target")
+
+		IsIP, _ := regexp.Match(IP_REGEX, []byte(Target))
+
+		if !IsIP {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, "Target parameter should be an valid IPv4 address like (192.168.10.100)")
+			return
+		}
+
+		if strings.ToLower(Query.Get("module")) != "arp" {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, "Invalid module parameter (Set this value to arp)")
+			return
+		}
+
+		inf, err := net.InterfaceByName(*InterfaceName)
+
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "The interface specified to send ARP requests does not exist (use ifconfig to find them)")
+			return
+		}
+
+		client, err := arp.Dial(inf)
+
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "The exporter does not have enough permissions to send ARP packets (CAP_NET_RAW required)")
+			return
+		}
+
+		client.SetDeadline(time.Now().Add(time.Millisecond * time.Duration(Timeout)))
+
+		start := time.Now()
+		result, err := client.Resolve(netip.MustParseAddr(Target))
+		duration := time.Since(start).Seconds()
+		Success := 0
+
+		if len(result) > 0 {
+			Success = 1
+		} else if err != nil {
+			Success = 0
+		}
+
+		w.WriteHeader(200)
+		fmt.Fprint(w, GenerateMetrics(duration, Success))
+	})
+
+	http.ListenAndServe(*Address, nil)
+}
